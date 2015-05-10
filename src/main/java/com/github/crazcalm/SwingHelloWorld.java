bf70ec082be2dc40088e675707bb7a1b47d51851
Crazcalm <crazcalm@gmail.com>
2015-05-10 16:36:39 -0400

    I added a panel, a label, and a button the to class

--- src/main/java/com/github/crazcalm/SwingHelloWorld.java
+++ src/main/java/com/github/crazcalm/SwingHelloWorld.java
@@ -11,12 +11,25 @@ public class SwingHelloWorld extends JFrame {
         new SwingHelloWorld();
     }
 
-    public SwingHelloWorld(){
+    public SwingHelloWorld() {
 
         this.setSize(200, 100);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setTitle("Hello World");
+        this.setTitle("Hello World SwingHelloWorld...");
+
+        JPanel panel1 = new JPanel();
+        JLabel label1 = new JLabel("This is a label");
+        label1.setText("Text set in the label");
+
+        JButton button1 = new JButton();
+        button1.setText("Button says click me!");
+        panel1.add(label1);
+        panel1.add(button1);
+        this.add(panel1);
+
+
         this.setVisible(true);
 
     }
 }
+
