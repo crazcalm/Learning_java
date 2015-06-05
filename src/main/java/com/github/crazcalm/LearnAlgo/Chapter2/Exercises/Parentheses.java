cd5d60e225d616c611f8ff2ee521c4554ed15abb
Crazcalm <Crazcalm@gmail.com>
2015-06-04 23:05:46 -0400

    I refactered the Parentheses code, but I still do not like the API. I still do not know how to go about properly creating an api in java

--- src/main/java/com/github/crazcalm/LearnAlgo/Chapter2/Exercises/Parentheses.java
+++ src/main/java/com/github/crazcalm/LearnAlgo/Chapter2/Exercises/Parentheses.java
@@ -5,18 +5,34 @@ import com.github.crazcalm.LearnAlgo.Chapter2.Exercises.Stack;
  * Created by marcus on 6/3/15.
  */
 public class Parentheses {
-    public static Stack<String> list = new Stack<String>();
-    public static boolean match = true;
+    private static Stack<String> list = new Stack<String>();
+    private static boolean match = true;
 
     public static void main(String[] args){
+        String one = "(){}[]";
+        String two = ")({}";
+        String three = "({}[])";
+        String four = "((()))))";
 
-        Parentheses test = new Parentheses();
-        String testing = "[()]{}{[()()]()}";
+        String[] testsCases = {one, two, three, four};
+
+        System.out.println("Are these string balanced?\n");
+        for(String i: testsCases){
+            System.out.println(i + ": " + Parentheses.isBalanced(i));
+        }
+        System.out.print(three + ": " + Parentheses.isBalanced(three));
+    }
+
+    public static boolean isBalanced(String testing){
+        /**
+        This is a static method that takes a String a returns whether or not the
+         the brackets -- (), {}, [] -- are balanced.
+         */
         int limit = testing.length();
         int index = 0;
 
-        System.out.println();
-
+        // Need to make sure that the match boolean is reset before this method is ran.
+        Parentheses.reset();
 
         while(index < limit && match){
             String s = testing.substring(index, index + 1);
@@ -25,31 +41,25 @@ public class Parentheses {
                     s.equals("[")
                     ){
                 list.push(s);
-                System.out.print("\n\nPushed: " + s);
             }
             else if(s.equals(")") ||
                     s.equals("}") ||
                     s.equals("]")
                     ){
-                test.check(s);
+                Parentheses.check(s);
             }
             index++;
         }
 
-        if(test.answer()){
-            System.out.println("\n\nTrue");
-        }else{
-            System.out.println("\n\nFalse");
-        }
+        return Parentheses.answer();
     }
 
-    public void check(String item){
+    private static void check(String item){
         if(list.isEmpty()){
             match = false;
         }
         else{
             String temp = list.pop();
-            System.out.println("\n\nPopped: " + temp);
 
             if(temp.equals("(") && item.equals(")")) {
 
@@ -67,12 +77,16 @@ public class Parentheses {
 
     }
 
-    public boolean answer(){
+    private static boolean answer(){
         boolean result = false;
 
         if(list.isEmpty() && match){
             result = true;
         }
         return result;
+    }
+
+    private static void reset(){
+        match = true;
     }
 }
